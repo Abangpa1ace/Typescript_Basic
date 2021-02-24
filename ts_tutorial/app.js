4d0559d66591fb6ad645e6fa95618f9c221fee8e
Abangpa1ace <dyek72731@gmail.com>
2021-02-24 18:33:53 +0900

    Typescript(땅콩코딩) 실습

--- ts_tutorial/app.js
+++ ts_tutorial/app.js
@@ -1,4 +1,38 @@
-var logName = function (name) {
-    console.log(name);
-};
-logName('Taeng');
+var studentId = 123;
+var studentName = 'Taeng';
+var graduated = false;
+function getStudent(studentId) {
+    return {
+        studentId: 124,
+        studentName: 'Hyung',
+        graduated: true
+    };
+}
+function getStudentInfo(studentId) {
+    return {
+        studentId: 10,
+        studentName: 'Taeng',
+        age: 29,
+        subject: 'Typescript',
+        graduated: false
+    };
+}
+// function modifyStudentInfo(student: StudentInfo): void {
+//   student.studentId = 40,
+// }
+// enum
+var Subject;
+(function (Subject) {
+    Subject[Subject["Java"] = 0] = "Java";
+    Subject[Subject["Javascript"] = 1] = "Javascript";
+    Subject[Subject["Typescript"] = 2] = "Typescript";
+})(Subject || (Subject = {}));
+function getStudentInfoEnum(studentId) {
+    return {
+        studentId: 10,
+        studentName: 'Taeng',
+        age: 29,
+        subject: Subject.Typescript,
+        graduated: false
+    };
+}
--- ts_tutorial/app.ts
+++ ts_tutorial/app.ts
@@ -1,5 +1,112 @@
-const logName = (name: string) => {
-  console.log(name);
+let studentId = 123
+let studentName = 'Taeng'
+let graduated = false;
+
+function getStudent(studentId: number): object {
+  return {
+    studentId: 124,
+    studentName: 'Hyung',
+    graduated: true,
+  }
 }
 
-logName('Taeng');
+// function getStudentMoreInfo(studentId: number): {
+//   studentId: number,
+//   studentName: string,
+//   age: number,
+//   subject: string,
+//   graduated: boolean,
+// } {
+//   return null;
+// }
+
+// Interface 적용
+interface StudentInfo {
+  readonly studentId: number,
+  studentName: string,
+  age: number,
+  subject: string,
+  graduated: boolean,
+}
+
+function getStudentInfo(studentId: number): StudentInfo {
+  return {
+    studentId: 10,
+    studentName: 'Taeng',
+    age: 29,
+    subject: 'Typescript',
+    graduated: false,
+  }
+}
+
+// function modifyStudentInfo(student: StudentInfo): void {
+//   student.studentId = 40,
+// }
+
+// enum
+enum Subject {
+  Java,
+  Javascript,
+  Typescript
+}
+
+interface StudentInfoEnum {
+  readonly studentId: number,
+  studentName: string,
+  age: number,
+  subject: Subject,
+  graduated: boolean,
+}
+
+function getStudentInfoEnum(studentId: number): StudentInfoEnum {
+  return {
+    studentId: 10,
+    studentName: 'Taeng',
+    age: 29,
+    subject: Subject.Typescript,
+    graduated: false,
+  }
+}
+
+interface StudentInfoLiteral {
+  readonly studentId: number,
+  studentName: string,
+  age: number,
+  subject?: 'java' | 'javascript' | 'typescript',
+  graduated: boolean,
+}
+
+function getStudentInfoLiteral(studentId: number): StudentInfoLiteral {
+  return {
+    studentId: 10,
+    studentName: 'Taeng',
+    age: 29,
+    // subject: "python",
+    graduated: false,
+  }
+}
+
+let unionPrice: string | number = 5;
+unionPrice = '5';
+// unionPrice = false;
+
+// Type Guard
+type StrOrNum = string | number;
+let itemPrice: number;
+
+const setItemPrice = (price: StrOrNum): void {
+  if (typeof price === 'string') {
+    itemPrice = 0;
+  }
+  else {
+    itemPrice = price;
+  }
+}
+
+// Function Type
+function sendGreeting(message: string, userName?: string): void {
+  console.log(`${message}, ${userName}`);
+}
+
+sendGreeting('hello');
+
